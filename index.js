df542bb0a69dc2d7b0f560493adb812d864ca1e5
Enrique Georg Erbs <enriquegeorg@gmail.com>
2020-01-10 14:24:16 -0300

    commented a call to a function unimplemented

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    lista: require('./lista/lista.marko'),
+    form: require('./form/form.marko'),
+    valoresperiodo: require('../vagas/lista/dados.marko'),
+    //visualiza: require('../views/vagas/lista/view.marko')
+}
